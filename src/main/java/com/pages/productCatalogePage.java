4445f8db3a03bc4bc053e2341de8e7c898268774
sudarshan <sudarshankillekar1998@gmail.com>
2024-12-17 23:48:27 +0530

    added

--- src/main/java/com/pages/productCatalogePage.java
+++ src/main/java/com/pages/productCatalogePage.java
@@ -1,6 +1,6 @@
 package com.pages;
 
-import java.time.Duration;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -8,8 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import utils.BrowserUtility;
 
